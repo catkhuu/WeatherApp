b4e1df63e7c11b9b148518c3e8d5fb3bfca3d184
Catherine Khuu <sunshine.dumbo@gmail.com>
2017-06-11 21:21:05 -0400

    Create reducer_weather to fetch and return weather data for searched city. Create new container to display list of weather for different cities.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,22 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
   console.log('Action received', action);
+  // setup switch statement to only handle FETCH_WEATHER type
+
+  switch(action.type) {
+  case FETCH_WEATHER:
+      // accumulate past weather search for user and new weather data
+      // push will mutate state in reducer
+      // concat will not mutate state, instead
+      // it will return a new array with the previous and new weather data
+      // *** return state.concat([action.payload.data]);
+      // same result as above but more concise
+      // create array that contains action.payload.data
+      // three dots precede state, so it must be an array
+      // add existing entries from state array
+      // into the array containing action.payload.data
+      return [ action.payload.data, ...state ];
+  }
   return state;
 }
